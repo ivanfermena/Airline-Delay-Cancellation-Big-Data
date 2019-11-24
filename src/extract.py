de3e9a30fa5b2d02835a450af0ad3afa26c12bb9
ivanfermena <ifernandez@nextdigital.es>
2019-11-24 14:03:03 +0100

    Refactorizado y limpiado el caso. Falta: ver diferentes delays porque se han producido y donde

--- src/extract.py
+++ src/extract.py
@@ -4,6 +4,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import pyspark.sql.functions as fn
 from pyspark.sql.functions import col
+
+#
+# TODO: Estudio de retrasos de los vuelos por origen y destino
+#
+# Uso pandas para mayor facilidad a la hora de tratar la extraccion y tratamiento
+# de los datos
+
 import pandas as pd
 
 # --------- DATA EXTRACTION ---------
@@ -14,25 +21,21 @@ sc = SparkContext()
 spark = SparkSession.builder.appName("Aircraft-Delay-Cancelation").getOrCreate()
 
 # Concatena todos los csvs uno detras de otro
-#df = spark.read.option("header", "true").csv("../data/2018.csv")
-
+# df = pd.read_csv("../data/*.csv")
 df = pd.read_csv("../data/2018.csv")
 
 print(df.info())
 
-#num_rows = df.count()
+num_rows = df.count()
 
-#print(num_rows)
-
-# He comprobado con 'df.dropna() y df.fillna(0)' que no hay na en la tabla
-# Por tanto los campos que no me interesan son los vacios
-#df_cleaned = df.fillna(0).filter(df["DEP_DELAY"] != "").filter(df["DEP_DELAY"] != " ")
+print("Numero de lineas extraidas en el dataset:")
+print(num_rows)
 
 print("Numero de NA y null que tienen cada columna:")
 print (df.isna().sum())
 
 print("Exemplos de estos valores que no nos interesan:")
-##print(df[df['DEP_DELAY'].isna()])
+print(df[df['DEP_DELAY'].isna()])
 
 #missing_values = ["n/a", "na", "", " "]from pyspark.sql.functions import col
 #df_cleaned = pd.read_csv("../data/2018.csv", na_values = missing_values)
@@ -40,32 +43,30 @@ print("Exemplos de estos valores que no nos interesan:")
 df_cleaned = df[df['DEP_DELAY'].notna()]
 
 print("Revisamos el numero de NA y null que tienen cada columna:")
-##print(df_cleaned.isna().sum())
+print(df_cleaned.isna().sum())
 
 df_airports = spark.createDataFrame(df_cleaned[['ORIGIN','DEST','DEP_DELAY']])
 
-##print(df_airports.dtypes)
-
+# Comprobamos los tipos de las columnas para no tener error de typos en el sum
+print(df_airports.dtypes)
 
 # --------- DATA TRANSFORMATION ---------
 
+# Cogemos las columnas que nos interesan y las casteamos a Integer
 df_airports = df_airports.withColumn('DEP_DELAY', col("DEP_DELAY").cast(IntegerType()))
 
+# Agrupamos por Origen y destino
 df_gb_airports = df_airports.groupby(['ORIGIN', 'DEST'])
 
+# Hacemos la suma por del retraso a partir del grupo
 df_grouped = df_gb_airports.agg(fn.sum(col('DEP_DELAY')).alias('DEP_DELAY_SUM'))
 
-
-#df_airports = spark.createDataFrame(df[['ORIGIN','DEST','DEP_DELAY']])
-
-#print(df_airports.dtypes)
-
-#print(df_airports)
-
-#df_group_origin = df_airports.groupBy("ORIGIN")
+df_grouped = df_grouped.sort(fn.desc("DEP_DELAY_SUM"))
 
 print(df_grouped.show())
 
+df_grouped_pandas = df_grouped.toPandas()
+
 # --------- DATA LOAD ---------
 
-# df.toPandas().to_csv('mycsv.csv')
+df_grouped_pandas.to_csv('../data/load.csv')
